58d6f3ba02057a99484dd3aef14567dfe362daa1
SoytheProton <alttimelinemod@gmail.com>
2025-10-02 15:50:39 -0800

    Update CustomBottleRelic.java

--- mod/src/main/java/basemod/abstracts/CustomBottleRelic.java
+++ mod/src/main/java/basemod/abstracts/CustomBottleRelic.java
@@ -6,8 +6,10 @@ import java.util.function.Predicate;
 
 public interface CustomBottleRelic
 {
+	// Utilized for making relics appear on the Top Right of cards. Does nothing else in terms of "Bottles"
 	Predicate<AbstractCard> isOnCard();
 
+	// Base game bottles do not show in combat. It is set to default true however.
 	default boolean showInCombat(AbstractCard card) {
  		return true;
 	}
