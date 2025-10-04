07c8238e61ef20547355bbe4238be448ef12012b
SoytheProton <alttimelinemod@gmail.com>
2025-10-04 12:07:00 -0800

    Update RenderRelicOnCard.java

--- mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/RenderRelicOnCard.java
+++ mod/src/main/java/basemod/patches/com/megacrit/cardcrawl/cards/AbstractCard/RenderRelicOnCard.java
@@ -33,7 +33,7 @@ public class RenderRelicOnCard
 	{
 		if (!Settings.hideCards /*&& !__instance.isOnScreen()*/ && !__instance.isFlipped) {
 			for (Pair<Predicate<AbstractCard>, AbstractRelic> info : BaseMod.getBottledRelicList()) {
-				if (info.getKey().test(__instance) && (((CustomBottleRelic) info.getValue()).showInCombat(__instance) || (AbstractDungeon.getCurrRoom()).phase != AbstractRoom.RoomPhase.COMBAT)) {
+                if (info.getKey().test(__instance) && (((CustomBottleRelic) info.getValue()).showInCombat(__instance) || (AbstractDungeon.getCurrRoom()).phase != AbstractRoom.RoomPhase.COMBAT || AbstractDungeon.screen == AbstractDungeon.CurrentScreen.MASTER_DECK_VIEW)) {
 					AbstractRelic r = info.getValue();
 					r.scale = __instance.drawScale * Settings.scale * ((CustomBottleRelic) r).scale(__instance);
 					try {
